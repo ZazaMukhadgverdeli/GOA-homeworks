9be4eb21e3e67dd3c0774822085d9848c38ffe16
ZazaMukhadgverdeli <Mukhadgverdelizaza@gmail.com>
2024-05-22 21:39:04 +0400

    homework

--- day 01/homework/level 01.py
+++ day 01/homework/level 01.py
@@ -3,3 +3,14 @@ print("Ani Verulashvili")
 print("Tatuka Mukhadgverdeli")
 print("Teko Mukhadgverdeli")
 print("Zviadi Mukhadgverdeli")
+
+
+
+
+name = "Zaza"
+age  = 11
+height =1.51
+
+print (name)
+print(age)
+print (height)
