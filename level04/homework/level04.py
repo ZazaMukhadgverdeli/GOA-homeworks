e894dbb73e2c35144d5f150d4bbb7f7712be3e4c
ZazaMukhadgverdeli <Mukhadgverdelizaza@gmail.com>
2024-06-03 09:52:10 +0400

    lesson

---
+++ level04/homework/level04.py
@@ -0,0 +1,23 @@
+print("555*5")
+print("275*954")
+print("4125*3642")
+print("1752*3917")
+print("1428*92783")
+print("75146*3126")
+print("13524*27245")
+print("1452455*245214")
+print("414528*1725")
+print("7175*932745")
+print("41*3245")
+print("145245*45757")
+print("5245*414")
+print("4145*52152")
+print("414514*5747")
+print("1414378*4714441")
+
+Name = "Zaza"
+Last_name = "Mukhadgverdeli"
+Age = "11"
+Hobby = "Programing"
+Country = "Georgia"
+ 
