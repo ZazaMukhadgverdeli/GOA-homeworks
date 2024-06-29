bab71d2c2aec6802b9b4e2daccee3efbde4afd74
ZazaMukhadgverdeli <Mukhadgverdelizaza@gmail.com>
2024-06-29 15:24:25 +0400

    homework

---
+++ level 07/homework/level07.py
@@ -0,0 +1,53 @@
+#N1
+a = int(input("enter first number;"))
+b = int(input("enter second number"))
+c = int(input("enter third number"))
+d = int(input("enter fourth number"))
+
+print(a * b)
+print(a // c)
+print(a + d)
+print(a - a)
+print(b * a)
+print(b // c)
+print(b + d)
+print(b - b)
+print(c * b)
+print(c // a)
+print(c + d)
+print(c - c)
+print(d * b)
+print(d // c)
+print(d + a)
+print(d - d)
+
+
+
+
+
+#N2
+mother = int(input("enter my mom's age;"))
+dad = int(input("enter my dad's age"))
+sister = int(input("enter my sister's age"))
+sister2 = int(input("enter my sisters's age"))
+after_20 = 20
+result = (mother + after_20)
+print(result)
+result2 = (dad + after_20)
+print(result2)
+result3 =(sister + after_20)
+print(result3)
+result4 = (sister2 + after_20)
+print(result4)
+
+
+
+#N3
+name = 'my name is Zaza'
+last_name = 'my last name is Mukhadgverdeli'
+age = 'I am 11 years old'
+country = 'I live in Georgia'
+favorite_car = 'my favorite car is Zhiguli'
+favorite_color = 'my favorite colors are red and green'
+favorite_number = 'my favorite number is 76'
+print(name,last_name,age,country,favorite_car,favorite_number)
