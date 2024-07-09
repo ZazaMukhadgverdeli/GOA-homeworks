376cda0b891f48e0c1d48c304d5aaa02fa0cb92f
ZazaMukhadgverdeli <Mukhadgverdelizaza@gmail.com>
2024-07-09 14:13:35 +0400

    level

---
+++ level09.py/homework/levelo92.py
@@ -0,0 +1,8 @@
+# დაწერეთ პროგრამა, რომელიც იღებს ორი ადამიანის ასაკს და განსაზღვრავს ვინ არის უფროსი.
+
+Zaza = int(input("enter Zaza's age"))
+Ani = int(input("Ani's age"))
+
+they = (Zaza<Ani)
+print(they)
+
