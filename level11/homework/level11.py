eb880015d7e29fb5678f95c7b9e6a76773edbaf8
ZazaMukhadgverdeli <Mukhadgverdelizaza@gmail.com>
2024-07-23 16:17:48 +0400

    second commit

---
+++ level11/homework/level11.py
@@ -0,0 +1,165 @@
+favorite_food = input("შემოიტანეთ თქვენი საყვარელი ხილი: ")
+
+if favorite_food == "მანგო":
+    print(True)
+else:
+    print(False)
+
+
+
+
+
+
+
+    my_string = ""
+if my_string == "":
+    print(True)
+else:
+    print(False)
+
+
+my_string = "Hello"
+if my_string != "":
+    print(True)
+else:
+    print(False)
+
+
+a = 5
+b = 10
+if a < b:
+    print(True)
+else:
+    print(False)
+
+
+x = 5
+y = 5
+if x == y:
+    print(True)
+else:
+    print(False)
+
+
+a = 5
+b = 10
+c = 15
+if a < b < c:
+    print(True)
+else:
+    print(False)
+
+
+a = 0
+b = 10
+if a or b:
+    print(True)
+else:
+    print(False)
+
+
+a = True
+b = False
+if not b:
+    print(True)
+else:
+    print(False)
+
+
+a = 5
+b = 10
+if a != b:
+    print(True)
+else:
+    print(False)
+
+
+a = 7
+if a % 2 != 0:
+    print(True)
+else:
+    print(False)
+
+
+a = 6
+if a % 2 == 0:
+    print(True)
+else:
+    print(False)
+
+a = 5
+b = 10
+if a > b:
+    print(True)
+else:
+    print(False)
+
+
+a = True
+b = False
+if a and  b:
+    print(True)
+else:
+    print(False)
+
+
+x = 5
+y = 10
+if x != y:
+    print(True)
+else:
+    print(False)
+
+
+a = 5
+b = 2
+if a > 3 and b < 4:
+    print(True)
+else:
+    print(False)
+
+
+a = 5
+b = 10
+if not a > b:
+    print(True)
+else:
+    print(False)
+
+
+text = "Hello"
+if 'e' in text:
+    print(True)
+else:
+    print(False)
+
+
+text = "Hello"
+if 'x' not in text:
+    print(True)
+else:
+    print(False)
+
+
+age = 25
+if age >= 18:
+    print(True)
+else:
+    print(False)
+
+
+age = 10
+if age >= 8:
+    print(True)
+else:
+    print(False)
+
+
+
+
+age = 11
+if age >= 1:
+    print(True)
+else:
+    print(False)
+
